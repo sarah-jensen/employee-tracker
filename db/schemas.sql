cd0d48d8b0fc72e7ee6f86acebaf508d05daec46
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-04-30 16:21:01 -0700

    add schemas.sql

---
+++ db/schemas.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS staff_db;
+CREATE DATABASE staff_db;
+
+USE staff_db;
+
+CREATE TABLE  department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+    PRIMARY KEY (id),
+    FOREIGN kEY (role_id) REFERENCES role(id),
+)
---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const inquirer = require('inquirer')
+const app = express();
+
+const routes = require('./routes');
+const PORT = 3001;
+
+
