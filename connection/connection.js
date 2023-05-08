17245681f4c974a9949bd9bc04a226507f0b4e61
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-05-07 17:38:51 -0700

    add items in prompt-tree

---
+++ connection/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const employeeDb = mysql.createConnection(
+    {
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    },
+    console.log('Connected to employee database.')
+);
+
+
+module.exports = employeeDb;
---
+++ zoops.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+// {value: "View all departments",
+//             open: true,
+//             children: [
+//               {value: "Royalty" },
+//               {value: "Nobility" },
+//               {value: "Maia"},
+//               {value: "Military"},
+//               {value: "Common Folk"},
+//               {value: "Creatures"},
+//             ]
+//           },
+//           {
+//             value: "View all roles",
+//             open: true,
+//             children: [
+//               {value: "King"},
+//               {value: "Steward"},
+//               {value: "Lord"},
+//               {value: "Lady"},
+//               {value: "Wizard"},
+//               {value: "Soldier"},
+//               {value: "Archer"},
+//               {value: "Inn Keeper"},
+//               {value: "Ring Bearer"},
+//               {value: "Gardener"},
+//               {value: "Trouble Maker"},
+//               {value: "Ent"},
+//               ]
+//             },
+//             {
+//               value: "View all employees",
+//               open: true,
+//               children: [
+//                 {value: },
+//                 {value: },
+//                 {value: },
+//                 {value: },
+//                 {value: },
+//                 {value: },
+//                 {value: },
+//               ]
+//             }
+//             ]
+//           }
+
+
+//           const { inits, addDepartment, addRole, addEmployee, updateRole } = require('./utils/prompts.js');
