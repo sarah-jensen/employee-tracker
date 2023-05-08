c362bd33978428ebb7da807edeecc77446e75cfc
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-05-07 17:55:06 -0700

    Delete routes directory

--- routes/apiRoutes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes/index')
-const todoRoutes = require('./todoRoutes/index')
-
-const userRoutes = require('./userRoutes');
-
-const todoRoutes = require('./todoRoutes');
-
-module.exports = router;
